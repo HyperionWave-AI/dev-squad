b986df4b1daf4e01a872fd389d53298456279bdf
ashalainko <andrii@hyperionwave.com>
2025-11-03 11:47:47 +0200

    Add ui for monitoring indexing files
    Former-commit-id: 175701d00b8bdd65c38bf80e89298fe3a1172916

--- ui/src/components/code/index.ts
+++ ui/src/components/code/index.ts
@@ -3,3 +3,5 @@ export { CodeIndexConfig } from './CodeIndexConfig';
 export { CodeSearch } from './CodeSearch';
 export { CodeResults } from './CodeResults';
 export { IndexStatus } from './IndexStatus';
+export { FileInspector } from './FileInspector';
+export { ChunkList } from './ChunkList';
